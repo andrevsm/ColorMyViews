3642dead2518da4772b2943c1b420827bcf22093
Andre Melo <andrevsm@Andres-MacBook-Air.local>
2021-06-05 11:48:40 -0300

    Conclude color app

--- app/src/main/java/br/com/avsm/colormyviews/MainActivity.kt
+++ app/src/main/java/br/com/avsm/colormyviews/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
-                constraint_layout)
+                constraint_layout, green_button, yellow_button, red_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -25,16 +25,15 @@ class MainActivity : AppCompatActivity() {
 
     fun makeColored(view: View) {
         when (view.id) {
-
-            // Boxes using Color class colors for background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-
-            // Boxes using Android color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+            
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
